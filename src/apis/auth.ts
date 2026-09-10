bb65a01659efb4855f98afe73ab31802684de5fa
kxxnayun <nayun2179@gmail.com>
2026-09-10 10:57:40 +0900

    feat: 주석 수정 (#3)

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,8 +1,7 @@
 export type SocialProvider = "kakao" | "google";
 
 // 백엔드 OAuth 시작점으로 리다이렉트한다.
-// TODO: 백엔드 인증 명세 확정되면 이 함수만 교체하면 된다.
-//       (프론트 SDK로 토큰을 직접 받는 방식이면 시그니처가 Promise로 바뀔 수 있음)
+// TODO: 백엔드 인증 명세 확정되면 이 함수만 교체하면 된다
 export function startSocialLogin(provider: SocialProvider) {
   const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
