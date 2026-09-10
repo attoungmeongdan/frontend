8fbfc37f0c0bcbe23516d002f4099487dd7c5e33
이재인 <jilee0818@gmail.com>
2026-09-11 01:28:27 +0900

    feat: 쿼리 기본 옵션 설정 (#30)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
+import { queryClient } from "@/config/queryClient";
 import { router } from "@/routes/router";
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
